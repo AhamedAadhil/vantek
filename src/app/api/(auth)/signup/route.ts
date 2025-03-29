d65850dc382bcf3f31500c2d3e6b899da6779adc
Ahamed Aathil <ahamedaathil.5@gmail.com>
2025-03-29 13:30:17 +0530

    backend: complete login and register
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- src/app/api/(auth)/signup/route.ts
+++ src/app/api/(auth)/signup/route.ts
@@ -1,6 +1,7 @@
 import connectDB from "@/lib/db";
 import User from "@/lib/models/user";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
@@ -25,6 +26,7 @@ export const POST = async (req: Request) => {
         { status: 400 }
       );
     }
+    // hash password
     const hashedPassword = await bcrypt.hash(password, 12);
 
     const newUser = new User({
@@ -32,13 +34,34 @@ export const POST = async (req: Request) => {
       password: hashedPassword,
       name,
     });
-    // TODO: gen jwt
+
+    // gen jwt
+    const token = jwt.sign(
+      {
+        email: newUser.email,
+        id: newUser._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
     await newUser.save();
     const { password: _, ...user } = newUser._doc;
-    return NextResponse.json(
+
+    const response = NextResponse.json(
       { message: "Account created", data: user, success: true },
       { status: 201 }
     );
+
+    // set cookie
+    response.cookies.set("token", token, {
+      httpOnly: true, // Prevents client-side access
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      sameSite: "strict", // Prevents CSRF attacks
+      maxAge: 7 * 24 * 60 * 60, // 7 days expiration
+    });
+
+    return response;
   } catch (error) {
     return NextResponse.json(
       {
