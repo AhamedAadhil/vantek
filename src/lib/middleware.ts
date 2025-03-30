537f17b57df90a83e9af58f263d9ddb6a9159573
Ahamed Aathil <ahamedaathil.5@gmail.com>
2025-03-30 12:15:47 +0530

    backend: create auth middleware
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

---
+++ src/lib/middleware.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = async (req: NextRequest) => {
+  try {
+    const token = req.cookies.get("token")?.value;
+    if (!token) {
+      return NextResponse.json(
+        { message: "Unauthorized", success: false },
+        { status: 401 }
+      );
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+      userId: string;
+      email: string;
+    };
+    if (!decoded) {
+      return NextResponse.json(
+        { message: "Unauthorized or Invalid Token", success: false },
+        { status: 401 }
+      );
+    }
+    // ✅ Manually attach user details to request headers
+    req.headers.set("userId", decoded.userId);
+    req.headers.set("email", decoded.email);
+
+    // If authentication is successful, return null (no error response)
+    return null;
+
+    /*
+    this following approach is for global middleware
+    const requestHeaders = new Headers(req.headers);
+    requestHeaders.set("userId", decoded.userId);
+    requestHeaders.set("email", decoded.email);
+    return NextResponse.next({ request: { headers: requestHeaders } });
+    */
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Expired or Invalid Token", success: false },
+      { status: 401 }
+    );
+  }
+};
